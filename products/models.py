acce5a9e635eea1671d2b6af8f0351d8d48d9cc5
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-11-27 17:55:46 +0530

    added product and int.. models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Ingredients(models.Model):
+    name = models.CharField( max_length=50)
+    percentage = models.IntegerField()
+
+class Products(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    price = models.IntegerField()
+    sellin_price = models.IntegerField()
+    quantity = models.IntegerField()
+    ingredients = models.ManyToManyField(Ingredients)
+
