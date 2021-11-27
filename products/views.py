a33d54d0b3be8777990e36f4588ac86c7bcd6192
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-11-27 17:56:08 +0530

    added curd operations

--- products/views.py
+++ products/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from .models import Ingredients, Products
+from .serializers import ProductSerializer
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework import status
 
-# Create your views here.
+@csrf_exempt
+@api_view(('GET','POST'))
+def products(request):
+    if request.method == 'GET':
+        snippets = Products.objects.all()
+        print(snippets)
+        serializer = ProductSerializer(snippets, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return HttpResponse("hello monster")
+
+
+@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
+def product(request, id):
+    try:
+        product = Products.objects.get(id=id)
+    except Products.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
