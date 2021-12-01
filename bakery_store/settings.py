73b9d04c15780eafac2f39f165b5d3b24aa7ccad
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-12-01 09:58:08 +0530

    auth congig added

--- bakery_store/settings.py
+++ bakery_store/settings.py
@@ -128,3 +128,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        
+    ]
+}
