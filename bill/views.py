8d1801b74e64cf7349dd4eaed480df45a51ea50d
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-12-01 09:56:08 +0530

    added auth

--- bill/views.py
+++ bill/views.py
@@ -5,14 +5,15 @@ from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from .models import Bill
 from .serializers import BillSerializer
-from rest_framework.decorators import api_view, renderer_classes, authentication_classes
+from rest_framework.decorators import api_view, renderer_classes, authentication_classes, permission_classes
 from rest_framework import status
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework.authentication import TokenAuthentication
 
 @csrf_exempt
 @api_view(('GET','POST'))
 @authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticatedOrReadOnly])
 def bills(request):
     if request.method == 'GET':
         snippets = Bill.objects.all()
@@ -26,9 +27,9 @@ def bills(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     return HttpResponse("hello monster")
 
-
-@csrf_exempt
-@api_view(['GET', 'PUT', 'DELETE'])
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def bill(request, id):
     try:
         product = Bill.objects.filter(user_id=int(id))
