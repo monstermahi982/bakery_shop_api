96ac7e8eab5a4843dd222a6fa1ab8093f3627b92
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-12-01 09:56:36 +0530

    auth issue fixed

--- products/views.py
+++ products/views.py
@@ -5,15 +5,18 @@ from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from .models import Ingredients, Products
 from .serializers import ProductSerializer
-from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.decorators import api_view, renderer_classes, authentication_classes, permission_classes
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 @csrf_exempt
 @api_view(('GET','POST'))
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticatedOrReadOnly])
 def products(request):
     if request.method == 'GET':
         snippets = Products.objects.all()
-        print(snippets)
         serializer = ProductSerializer(snippets, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
